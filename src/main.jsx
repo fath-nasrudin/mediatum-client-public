f59441a4281d5f69059b30a2a1f81d7a5857e5c4
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-29 21:21:30 +0700

    feat[routing]: Add client routing feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'articles', 
+    element: <App />,
+    children: [
+      {
+        path: ':id', 
+        element: <App />,
+      },
+    ]
+  },
+  
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}  />
   </React.StrictMode>,
 )
