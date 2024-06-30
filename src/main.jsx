290fea8be9f5b2e64addfccf17b4b85f9fa1ed73
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-30 20:36:54 +0700

    fix: redicect root page to /articles page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import App from './App.jsx'
 import ArticleItem from './pages/ArticleItem.jsx'
 import Layout from './pages/Layout.jsx'
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <App />,
+        element: <Navigate to={'articles'} /> ,
       },
       {
         path: 'articles', 
