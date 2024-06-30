28ddef8bf5face2e43cc89a9caf902b455c131a5
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-30 20:49:08 +0700

    feat[header]: Change the main header max-width

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,13 +2,16 @@ import { Outlet, Link } from "react-router-dom";
 const Header = () => {
   return (
     <header
-      className="p-4"
+fix      className="p-4 border-b-2"
     >
-      <Link to={'/'}>
-        <div className="text-xl font-bold tracking-wide">
-          Mediatum
-        </div>
-      </Link>
+      <div className="max-w-screen-xl mx-auto">
+        <Link to={'/'}>
+          <div className="text-xl font-bold tracking-wide">
+            Mediatum
+          </div>
+        </Link>
+      </div>
+
     </header>
   )
 };
