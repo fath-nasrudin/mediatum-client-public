abd7553bb92ef2514120613a97a6cc7081b5e7da
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-07-02 12:39:56 +0700

    feat[auth]: Add refresh token function

--- src/utilites/authentication/AuthProvider.jsx
+++ src/utilites/authentication/AuthProvider.jsx
@@ -58,8 +58,44 @@ const AuthProvider = ({ children }) => {
     localStorage.removeItem('access_token');
   };
 
+  const refreshToken = async () => {
+    try {
+      console.log('try to refresh token...');
+      const response = await fetch(`${config.server.url}/auth/refresh-token`, {
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        console.log('refresh token response not ok');
+        // notify user that the session has end
+
+        console.log('try to logout');
+        const errorResult = await response.json();
+        throw new Error(errorResult.message);
+      }
+
+      console.log('success to get a new access token');
+      const result = await response.json();
+
+      console.log({ token, decoded: jwtDecode(result.access_token) });
+
+      // update token and user
+      console.log('updating user and token state...');
+      setUser(jwtDecode(result.access_token));
+      localStorage.setItem('access_token', result.access_token);
+      setToken(result.access_token);
+      console.log('success update user and token state');
+      return { error: null };
+    } catch (error) {
+      logoutAction();
+      console.error('Error fetching data:', error);
+      return { error };
+    }
+  };
   return (
-    <AuthContext.Provider value={{ user, token, loginAction, logoutAction }}>
+    <AuthContext.Provider
+      value={{ user, token, loginAction, logoutAction, refreshToken }}
+    >
       {children}
       <Outlet />
     </AuthContext.Provider>
